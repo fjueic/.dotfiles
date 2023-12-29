f2158c7a79a3112b95bb6a07f017467f4b7852eb
Rohit <rohitmalik987789@gmail.com>
2023-12-30 00:39:18 +0530

    remove files

--- .config/autokey/data/Sample Scripts/New Script.py
+++
@@ -1,6 +0,0 @@
-# script for pasting
-from time import sleep
-
-for _ in range(20000):
-    mouse.click_relative_self(0, 0, 1)
-    
--- scripts/utils.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def run_command(command):
-    try:
-        output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
-        return output
-    except subprocess.CalledProcessError as e:
-        return f"Error: {e.returncode}\n{e.output}"
